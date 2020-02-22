04a4112c13e4197a2784c5660abbf6279435770b
Tasay <den1647ko@mail.ru>
2020-02-22 14:05:09 +0300

    added template for all progect pages, added landing page with styles

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -1,3 +1,3 @@
-import '../common.blocks/header/header.js'
-import '../common.blocks/footer/footer.js'
-import '../common.blocks/login-form/login-form.js'
+import '../../common.blocks/header/header.js'
+import '../../common.blocks/footer/footer.js'
+import '../../common.blocks/login-form/login-form.js'

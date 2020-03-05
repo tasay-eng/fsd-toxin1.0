a193e0e5597cfb31a243aa98f1ab90bf2c31c326
Tasay <den1647ko@mail.ru>
2020-03-05 16:56:49 +0300

    added new mixins to less config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './pages/landing-page/landing-page.js'
 import './pages/search-page/search-page.js'
+import './pages/sign-up-page/sign-up-page.js'
 import './pages/landing-page/landing-page.less'
 import './pages/search-page/search-page.less'
 import './pages/sign-up-page/sign-up-page.less'

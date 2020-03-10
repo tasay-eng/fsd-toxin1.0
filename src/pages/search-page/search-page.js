53712d78ff135d414fb90a5ad1525150d2566ecd
Tasay <den1647ko@mail.ru>
2020-03-10 15:46:29 +0300

    changed structure of search-page, added 1 card to cards-for-rooms

--- src/pages/search-page/search-page.js
+++ src/pages/search-page/search-page.js
@@ -1,3 +1,4 @@
 import '../../common.blocks/header/header.js'
 import '../../common.blocks/footer/footer.js'
-import '../../common.blocks/room-card/room-card.js'
+import '../../common.blocks/cards-for-rooms/cards-for-rooms.js'
+import '../../common.blocks/choose-options/choose-options.js'

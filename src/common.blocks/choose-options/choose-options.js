b0d9340fe51cf84f03274a6b998582ae9eb3b677
Tasay <den1647ko@mail.ru>
2020-03-10 15:43:27 +0300

    added working drop-down to blockes, + some basic styles

---
+++ src/common.blocks/choose-options/choose-options.js
@@ -0,0 +1,3 @@
+import '../small.blocks/dropdown/dropdown.js'
+import '../small.blocks/input/input.js'
+import '../small.blocks/button/button.js'
--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,2 +1,3 @@
 import '../small.blocks/button/button.js'
 import '../small.blocks/dropdown/dropdown.js'
+import '../small.blocks/dropdown/dropdown.js'

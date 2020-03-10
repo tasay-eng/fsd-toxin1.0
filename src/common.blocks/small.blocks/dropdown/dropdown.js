cf4220a983eff4d7beb2d545da479c501bac613b
Tasay <den1647ko@mail.ru>
2020-03-10 15:38:07 +0300

    drop-down.pug repared to working vertion, added js script for drop-down

--- src/common.blocks/small.blocks/dropdown/dropdown.js
+++ src/common.blocks/small.blocks/dropdown/dropdown.js
@@ -1,1 +1,10 @@
 import '../plus-minus/plus-minus.js'
+var sel = Array.from(document.querySelectorAll(".drop-down"))
+console.log(sel)
+for (let i of sel){
+    i.addEventListener("mousedown", appear(e))
+}
+function appear(e){
+    let drop = e.target.lastChild
+    drop.style.display= 'block'
+}

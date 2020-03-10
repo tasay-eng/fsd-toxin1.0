e10dea84ca6d2dc363109302e9e219bab18ea58d
Tasay <den1647ko@mail.ru>
2020-03-10 15:33:38 +0300

    updated all node-modules, deleted source-maps and babel from from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    resolve: {
+        extensions: ['.js']
+    },
     entry: {
         main: './src/main.js',
         'landing-page': './src/pages/landing-page/landing-page.js',
@@ -13,43 +16,24 @@ module.exports = {
 
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    devtool: false,
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
             {
                 test: /\.less$/,
                 exclude: /node_modules/,
                 use:  [
                      MiniCssExtractPlugin.loader,
                     {loader: 'css-loader',
-                     options: {
-                            sourceMap: true
-                        }
                     },
                     {loader: 'postcss-loader',
-                    options: { sourceMap: true, config: { path: 'postcss.config.js' } }
+                    options: { config: { path: 'postcss.config.js' } }
                     },
                     {loader: 'less-loader',
                     options: {
-                        sourceMap: true,
                         javascriptEnabled: true
                         }
                     }
@@ -58,7 +42,13 @@ module.exports = {
             {
                 test: /\.pug$/,
                 exclude: /node_modules/,
-                use:  ['pug-loader']
+                use:  [
+                    { loader: 'pug-loader',
+                      options: {
+                        javascriptEnabled: true
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(jpg|png|svg|jpeg)$/,
@@ -76,27 +66,25 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: './src/pages/sign-up-page/sign-up-page.pug',
-            chunks: ['main', 'sign-up-page'],
+            chunks: [ 'main', 'sign-up-page'],
             filename: './sign-up-page.html'
         }),
         new HtmlWebpackPlugin({
             template: './src/pages/search-page/search-page.pug',
-            chunks: ['main', 'search-page'],
+            chunks: [ 'main', 'search-page'],
             filename: './search-page.html'
         }),
         new HtmlWebpackPlugin({
             template: './src/pages/landing-page/landing-page.pug',
-            chunks: ['main', 'landing-page'],
+            chunks: [ 'main', 'landing-page'],
             filename: './landing-page.html'
         }),
         new CopyWebpackPlugin([
-            {from: './src/img', to: "./img"},
-            { from: './node_modules/jquery', to: `./node_modules/jquery`},
-            { from: './node_modules/jquery-ui', to: `./node_modules/jquery-ui`}
+            {from: './src/img', to: "./img"}
             ]
-        ),
-        new webpack.SourceMapDevToolPlugin({
-            filename: "[file].map"
-        }),
-    ]
-};
+        ),   
+    ],
+    devServer: {
+        stats: 'errors-only'
+    }
+}; 

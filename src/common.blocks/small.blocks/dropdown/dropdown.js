a953cdbed4524d554241cea3bf62ae9c2bde0167
Tasay <den1647ko@mail.ru>
2020-03-16 19:36:20 +0300

    added js code and changed layout of dropdown

--- src/common.blocks/small.blocks/dropdown/dropdown.js
+++ src/common.blocks/small.blocks/dropdown/dropdown.js
@@ -1,10 +1,24 @@
 import '../plus-minus/plus-minus.js'
-var sel = Array.from(document.querySelectorAll(".drop-down"))
-console.log(sel)
-for (let i of sel){
-    i.addEventListener("mousedown", appear(e))
+const drop = document.querySelectorAll(".drop-down_common")
+const drop_h = document.querySelectorAll(".drop-down_hidden")
+console.log(drop)
+console.log(drop_h)
+for (let i of drop){
+    i.addEventListener("click", appear)
+}
+for (let i of drop_h){
+    i.addEventListener("click", appear)
 }
 function appear(e){
-    let drop = e.target.lastChild
-    drop.style.display= 'block'
-}
+    let a = e.target.parentNode.lastChild
+    console.log(a)
+    console.log(e.target)
+    console.log(e.target.parentNode)
+    console.log(a.style.display)
+    if (a.style.display === 'none') {
+        a.style.display= 'block'
+    }
+    else {
+        a.style.display= 'none'
+    }
+}  

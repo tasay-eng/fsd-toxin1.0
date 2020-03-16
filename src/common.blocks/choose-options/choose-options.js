6d8458e2b5962e335710b8e851f2e83ff79dd747
Tasay <den1647ko@mail.ru>
2020-03-16 19:43:21 +0300

    changed styles of room-cards, added new slider to each of them, added datepicker to choose options

--- src/common.blocks/choose-options/choose-options.js
+++ src/common.blocks/choose-options/choose-options.js
@@ -1,3 +1,5 @@
 import '../small.blocks/dropdown/dropdown.js'
 import '../small.blocks/input/input.js'
 import '../small.blocks/button/button.js'
+import '../small.blocks/slider-range/slider-range.js'
+import '../small.blocks/dropdown/datepicker/_single-picker.js'
--- src/common.blocks/room-card/room-card.js
+++ src/common.blocks/room-card/room-card.js
@@ -1,1 +1,2 @@
 import '../small.blocks/rating-stars/rating-stars.js';
+import '../small.blocks/carusel-slider/carusel-slider.js';

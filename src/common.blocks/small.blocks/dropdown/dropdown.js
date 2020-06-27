0fc0e58f84ac982ae6dd49c5645242de90381f40
Tasya <den1647ko@mail.ru>
2020-06-27 15:47:03 +0300

    corrected drop-down plugin

--- src/common.blocks/small.blocks/dropdown/dropdown.js
+++ src/common.blocks/small.blocks/dropdown/dropdown.js
@@ -1,6 +1,11 @@
 import '../plus-minus/plus-minus.js'
-const drop = document.querySelectorAll(".drop-down_common")
-const drop_h = document.querySelectorAll(".drop-down_hidden")
+const drop = document.querySelectorAll(".drop-field")
+const drop_h = document.querySelectorAll(".drop-field")
+const drop_body = document.querySelectorAll(".drop-body")
+const nums_guests = document.querySelectorAll(".input-amount")
+const plus = document.querySelectorAll(".plus")
+const minus = document.querySelectorAll(".minus")
+const signs = plus.concat(minus)
 console.log(drop)
 console.log(drop_h)
 for (let i of drop){
@@ -9,16 +14,30 @@ for (let i of drop){
 for (let i of drop_h){
     i.addEventListener("click", appear)
 }
+for (let i in signs){
+    signs[i].addEventListener("click", show_choice)
+}
 function appear(e){
     let a = e.target.parentNode.lastChild
-    console.log(a)
-    console.log(e.target)
-    console.log(e.target.parentNode)
-    console.log(a.style.display)
-    if (a.style.display === 'none') {
-        a.style.display= 'block'
+    if(a.className === 'drop-body'){
+        if (a.style.display === 'none') {
+            a.style.display= 'block'
+        }
+        else {
+            a.style.display= 'none'
+        }
     }
-    else {
-        a.style.display= 'none'
+} 
+
+function show_choice(e){
+    if (e.target){
+        let nums_guests = document.querySelectorAll(".input-amount")
+        for (let j of nums_guests){
+            if(j>0){
+                e.target.parentNode.firstChild.textContent = e.target.firstChild.textContent
+            }
+        }
     }
-}  
+}
+
+
